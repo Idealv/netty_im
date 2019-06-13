5cd03978c512bfd8d2595e5ad24f596e2c691c60
IdealVillage <958986180@qq.com>
2019-06-13 18:29:34 +0800

    add heartBeat comments

--- src/main/java/com/ideal/netty_im/handler/IMIdleStateHandler.java
+++ src/main/java/com/ideal/netty_im/handler/IMIdleStateHandler.java
@@ -6,8 +6,11 @@ import io.netty.handler.timeout.IdleStateHandler;
 
 import java.util.concurrent.TimeUnit;
 
-//客户端和服务端都需要检测对方是否假死
-//定期服务端和客户端发心跳信息:防止客户端没断连接，只是没发数据包的情况
+//连接假死:
+//1.假死:连接断开，但是另一端没有收到四次挥手数据包或RST数据包
+//2.非假死,但没发数据包
+//解决1:客户端和服务端都需要检测对方是否假死
+//解决2:定期服务端和客户端发心跳信息:防止客户端没断连接，只是没发数据包的情况
 //这里自动定期发送心跳信息，防止这种情况
 public class IMIdleStateHandler extends IdleStateHandler {
 
